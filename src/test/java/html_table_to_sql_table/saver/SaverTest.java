7168e5dc389a1b9bc106e91bc2bfc0bac843fa6a
pfcui <cuipengfei2008@gmail.com>
2019-04-22 00:02:14 +0800

    should not have commited this little bit of wrong code

--- src/test/java/html_table_to_sql_table/saver/SaverTest.java
+++ src/test/java/html_table_to_sql_table/saver/SaverTest.java
@@ -9,18 +9,12 @@ import org.jooq.impl.DSL;
 import org.junit.After;
 import org.junit.Test;
 
-import java.time.LocalDate;
-import java.time.YearMonth;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-
 import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.jooq.impl.DSL.*;
 import static org.jooq.impl.SQLDataType.DATE;
 import static org.jooq.impl.SQLDataType.INTEGER;
-
 
 public class SaverTest {
 	
@@ -37,7 +31,9 @@ public class SaverTest {
 		FormalizedData formalizedData = new
 			FormalizedData("TableA",
 			asList(new Column("F1", INTEGER), new Column("F2", DATE)),
-			asList(asList(1, java.sql.Date.valueOf(YearMonth.parse("January 2010", DateTimeFormatter.ofPattern("MMMM yyyy")).atDay(1))), asList(2, "1990-01-01"))
+			asList(
+				asList(1, "2019-01-07"),
+				asList(2, "1990-01-01"))
 		);
 		
 		//when
@@ -51,16 +47,8 @@ public class SaverTest {
 			.fetch();
 		
 		assertThat(fetch.getValue(0, 0), is(1));
-		assertThat(fetch.getValue(0, 1), is("a"));
+		assertThat(fetch.getValue(0, 1).toString(), is("2019-01-07"));
 		assertThat(fetch.getValue(1, 0), is(2));
-		assertThat(fetch.getValue(1, 1), is("b"));
-	}
-	
-	@Test
-	public void name1() {
-		String string = "January 2010";
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy", Locale.ENGLISH);
-		LocalDate date = LocalDate.parse(string, formatter);
-		System.out.println(date); // 2010-01-02
+		assertThat(fetch.getValue(1, 1).toString(), is("1990-01-01"));
 	}
 }
