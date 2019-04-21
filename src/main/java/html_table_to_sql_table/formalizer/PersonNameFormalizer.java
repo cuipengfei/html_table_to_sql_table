ff63aca67a07b822cf8800014137a6dcc829842f
pfcui <cuipengfei2008@gmail.com>
2019-04-21 23:36:48 +0800

    person name formalizer

---
+++ src/main/java/html_table_to_sql_table/formalizer/PersonNameFormalizer.java
@@ -0,0 +1,26 @@
+package html_table_to_sql_table.formalizer;
+
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.reverse;
+import static java.util.stream.Collectors.joining;
+
+public class PersonNameFormalizer extends Formalizer {
+	@Override
+	protected void formalize() {
+		convertCells(
+			column -> column.getName().contains("name"),
+			nameCell -> {
+				String noSingleQuote = nameCell.replace("'", "");
+				List<String> nameParts = asList(noSingleQuote.split(","));
+				reverse(nameParts);
+				
+				return nameParts.stream()
+					.map(String::trim)
+					.collect(joining(" "));
+			}
+		);
+	}
+}
---
+++ src/test/java/html_table_to_sql_table/formalizer/PersonNameFormalizerTest.java
@@ -0,0 +1,49 @@
+package html_table_to_sql_table.formalizer;
+
+import html_table_to_sql_table.FormalizedData;
+import html_table_to_sql_table.RawData;
+import org.junit.Test;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class PersonNameFormalizerTest {
+	@Test
+	public void first_name_should_be_before_last_name() {
+		//given
+		RawData rawData = new RawData(
+			"A",
+			asList("XYZ Name"),
+			asList(asList("Aaron Jr, John Bland")));
+		
+		//when
+		FormalizedData formalizedData = new ColumnsFormalizer()
+			.init(rawData)
+			.next(new PersonNameFormalizer())
+			.run();
+		
+		//then
+		String convertedName = formalizedData.getRows().get(0).get(0).toString();
+		assertThat(convertedName, is("John Bland Aaron Jr"));
+	}
+	
+	@Test
+	public void should_remove_single_quote_in_name() {
+		//given
+		RawData rawData = new RawData(
+			"A",
+			asList("XYZ Name"),
+			asList(asList("Aa'ronson, Joel Steven")));
+		
+		//when
+		FormalizedData formalizedData = new ColumnsFormalizer()
+			.init(rawData)
+			.next(new PersonNameFormalizer())
+			.run();
+		
+		//then
+		String convertedName = formalizedData.getRows().get(0).get(0).toString();
+		assertThat(convertedName, is("Joel Steven Aaronson"));
+	}
+}
